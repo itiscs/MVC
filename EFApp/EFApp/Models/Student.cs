293671babfba40a23af1fb17e29ff36f3532ce9b
moudroff <moudroff>
2018-03-01 16:57:17 +0300

    EF App

---
+++ EFApp/EFApp/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EFApp.Models
+{
+    [Table("NewStudents")]
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage ="Нельзя оставлять поле пустым")]
+        [MaxLength(50,ErrorMessage ="Строка должна иметь длину не более 50 символов")]
+        [Display(Name ="Фамилия")]
+        public string LastName { get; set; }
+        [MaxLength(50)]
+        [Display(Name = "Имя")]
+        public string FirstMidName { get; set; }
+        [Display(Name = "Дата записи на курсы")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
