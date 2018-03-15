7c53540ceb612a231250d667d4b63144d0e2ee2e
moudroff <moudroff>
2018-03-15 21:12:18 +0300

    EShop 1.0

---
+++ EShopApp/EShopApp/Models/Phone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EShopApp.Models
+{
+    public class Phone
+    {
+        public int ID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public string ImageFile { get; set; }
+        public int Amount { get; set; }
+    }
+}
